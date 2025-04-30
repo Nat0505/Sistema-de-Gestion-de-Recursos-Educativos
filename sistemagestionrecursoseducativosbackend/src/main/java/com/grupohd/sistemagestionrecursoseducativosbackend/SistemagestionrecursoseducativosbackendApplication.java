081174e575af89f50a381ee4fe43a3fe9bb8f317
Nataly <natalyramosm@gmail.com>
2025-04-30 03:07:39 -0500

    Proyecto inicial - backend

---
+++ sistemagestionrecursoseducativosbackend/src/main/java/com/grupohd/sistemagestionrecursoseducativosbackend/SistemagestionrecursoseducativosbackendApplication.java
@@ -0,0 +1,13 @@
+package com.grupohd.sistemagestionrecursoseducativosbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemagestionrecursoseducativosbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemagestionrecursoseducativosbackendApplication.class, args);
+	}
+
+}
---
+++ sistemagestionrecursoseducativosbackend/src/test/java/com/grupohd/sistemagestionrecursoseducativosbackend/SistemagestionrecursoseducativosbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupohd.sistemagestionrecursoseducativosbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemagestionrecursoseducativosbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
