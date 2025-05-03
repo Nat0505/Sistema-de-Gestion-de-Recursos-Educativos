69ce3b918d56c300b0ea43195b2e66ac312070a3
Roman <franciscoromero071004@gmail.com>
2025-05-03 11:59:43 -0500

    Configura rutas para estudiante, docente y vistas exclusivas de admin

--- sistemagestionrecursoseducativosfrontend/src/App.js
+++ sistemagestionrecursoseducativosfrontend/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Navbar from './components/common/Navbar';
+import LoginPage from './components/auth/Login';
+import RegistrationPage from './components/auth/Registro';
+import ServicioAuthUser from './components/service/ServicioAuthUser';
+import ActuUser from './components/userpage/ActuUser';
+import ServicioUser from './components/userpage/ServicioUser';
+import ProfilePage from './components/userpage/Perfil';
+
+import InicioEstudiante from './components/userpage/InicioEstudiante'; 
+import InicioDocente from './components/userpage/InicioDocente';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={<LoginPage />} />
+            <Route exact path="/login" element={<LoginPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/home" element={<InicioEstudiante />} />
+            <Route path="/home-docente" element={<InicioDocente />} />
+
+            {ServicioAuthUser.adminOnly() && (
+              <>
+                <Route path="/register" element={<RegistrationPage />} />
+                <Route path="/admin/user-management" element={<ServicioUser />} />
+                <Route path="/update-user/:userId" element={<ActuUser />} />
+              </>
+            )}
+            <Route path="*" element={<Navigate to="/login" />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
